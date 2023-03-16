f34a1fb97347f158445739a0d4cf240fbf80d4f7
Dima12A <qadmitriitest@gmail.com>
2023-03-16 17:00:45 +0200

    looks like done)

---
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -0,0 +1,36 @@
+package org.example.homework_nr_1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
+        boolean JavaIsAmazing = true;
+        float Pi = 3.14159F;
+
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterInAlphabet);
+        System.out.println(JavaIsAmazing);
+        System.out.println(Pi);
+
+        double Price = 9.12352F;
+        double Tax = 1.2546F;
+        int Quantity = 256;
+        double Sum = 0F;
+
+        System.out.println(Price);
+        System.out.println(Tax);
+        System.out.println(Quantity);
+        System.out.println(Sum);
+
+        double total = Price + Tax * Quantity + Sum;
+
+        System.out.println(total);
+    }
+}
+
+
